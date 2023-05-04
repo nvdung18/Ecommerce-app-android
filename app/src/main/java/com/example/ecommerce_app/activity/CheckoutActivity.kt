6a913282e24d252f7a6bda9f4530027f5d53e49e
MaiQuocHuy <huymq.21it@vku.udn.vn>
2023-05-04 15:53:51 +0700

    razopay

--- app/src/main/java/com/example/ecommerce_app/activity/CheckoutActivity.kt
+++ app/src/main/java/com/example/ecommerce_app/activity/CheckoutActivity.kt
@@ -1,36 +1,35 @@
 package com.example.ecommerce_app.activity
 
-import android.accounts.Account
 import android.app.Activity
-import android.content.ContentResolver
 import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.Adapter
 import android.widget.EditText
+import android.widget.RadioButton
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.room.ColumnInfo
-import com.airbnb.lottie.animation.content.Content
 import com.example.ecommerce_app.R
 import com.example.ecommerce_app.adapter.ProductCheckOutAdapter
 import com.example.ecommerce_app.databinding.ActivityCheckoutBinding
-import com.example.ecommerce_app.models.BrandAndModel
-import com.example.ecommerce_app.models.CartDetailsAndProduct
 import com.example.ecommerce_app.models.CartDetailsAndProductAndBranch
 import com.example.ecommerce_app.models.UserEntity
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.razorpay.Checkout
+import com.razorpay.PaymentResultListener
+import org.json.JSONObject
 import java.text.DecimalFormat
 
-class CheckoutActivity : AppCompatActivity() {
+
+class CheckoutActivity : AppCompatActivity(), PaymentResultListener {
     private lateinit var binding: ActivityCheckoutBinding
     lateinit var listProduct: ArrayList<CartDetailsAndProductAndBranch>
     lateinit var checkoutAdapter: ProductCheckOutAdapter
@@ -44,6 +43,8 @@ class CheckoutActivity : AppCompatActivity() {
     val uri_orderDetails: Uri = Uri.parse("content://com.example.admin/OrderDetails")
 
     private var addressCheckOut = ""
+    private var nameCheckOut = ""
+    private var phoneCheckOut = ""
     private var total: Double = 1.0
     private var voucher = ""
     private var method_payment = ""
@@ -80,6 +81,33 @@ class CheckoutActivity : AppCompatActivity() {
                     val address = result.data?.getStringExtra("address").toString()
                     if (address.isNotEmpty()) {
                         addressCheckOut = address
+                        binding.notificationTv.visibility = View.VISIBLE
+                    }
+                }
+            }
+
+        val getContentName =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val address = result.data?.getStringExtra("address").toString()
+                    if (address.isNotEmpty()) {
+                        nameCheckOut = address
+                        binding.nameTv.text = "Entered Name"
+                        val textColor = ContextCompat.getColor(this, R.color.primary)
+                        binding.nameTv.setTextColor(textColor);
+                    }
+                }
+            }
+
+        val getContentPhone =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val address = result.data?.getStringExtra("address").toString()
+                    if (address.isNotEmpty()) {
+                        phoneCheckOut = address
+                        binding.phoneTv.text = "Entered Phone"
+                        val textColor = ContextCompat.getColor(this, R.color.primary)
+                        binding.phoneTv.setTextColor(textColor);
                     }
                 }
             }
@@ -88,16 +116,26 @@ class CheckoutActivity : AppCompatActivity() {
             val intent = Intent(this, AddAddressActivity::class.java)
             getContent.launch(intent)
         }
-        //Voucher
 
+        binding.nameRecipient.setOnClickListener {
+            val intent = Intent(this, AddAddressActivity::class.java)
+            getContentName.launch(intent)
+        }
+
+        binding.phoneRecipient.setOnClickListener {
+            val intent = Intent(this, AddAddressActivity::class.java)
+            getContentPhone.launch(intent)
+        }
+
+        //Voucher
         binding.VoucherTv.setOnClickListener {
             val bottomSheetDialod = BottomSheetDialog(
                 this, R.style.BottomSheetDialogTheme
             )
 
             val bottomSheetView = LayoutInflater.from(applicationContext).inflate(
-                R.layout.fragment_add_to_voucher,
-                findViewById<ConstraintLayout>(R.id.bottomSheet)
+                com.example.ecommerce_app.R.layout.fragment_add_to_voucher,
+                findViewById<ConstraintLayout>(com.example.ecommerce_app.R.id.bottomSheet)
             )
 
             bottomSheetView.findViewById<View>(R.id.saveVoucher_Btn).setOnClickListener {
@@ -110,21 +148,6 @@ class CheckoutActivity : AppCompatActivity() {
             bottomSheetDialod.show()
         }
         //Method payment
-        val getMethodPayment =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                if (result.resultCode == Activity.RESULT_OK) {
-                    //get id payment
-                    method_payment = result.data?.getStringExtra("idpayment").toString()
-                    if (method_payment != "") {
-                        binding.tvPaymentMethod.text = "Thanh Toan Bang Momo"
-                    }
-                }
-            }
-
-        binding.tvPaymentMethod.setOnClickListener {
-            val intent = Intent(this@CheckoutActivity, PaymentMethodActivity::class.java)
-            getMethodPayment.launch(intent)
-        }
         //price
         //format price to VND
         val vndFormat = DecimalFormat("#,### VND")
@@ -137,7 +160,7 @@ class CheckoutActivity : AppCompatActivity() {
         total = checkoutAdapter.getPrice()
 
         binding.orderBtn.setOnClickListener {
-            if (listProduct.size != 0 && addressCheckOut.isNotEmpty()) {
+            if (listProduct.size != 0 && addressCheckOut.isNotEmpty() && nameCheckOut.isNotEmpty() && phoneCheckOut.isNotEmpty() && method_payment.isNotEmpty()) {
                 addToCheckout()
             } else {
                 Toast.makeText(this@CheckoutActivity, "Empty", Toast.LENGTH_SHORT).show()
@@ -146,45 +169,79 @@ class CheckoutActivity : AppCompatActivity() {
     }
 
     private fun addToCheckout() {
-//        val user = getUserByToken()
-//        val values = ContentValues().apply {
-//            put("recipientName", user!!.fullName)
-//            put("recipientEmail", user!!.email)
-//            put("recipientAddress", addressCheckOut)
-//            put("total", total)
-//            put("idAccount", getCurrentIdAccount())
-//        }
-//        val uri_user_checkout = contentResolver.insert(uri_checkout, values)
-        if (voucher == "promo_01" || voucher == "") {
-            insertToCheckout()
-            val idOrder = insertToOrder()
-            insertToOrderDetails(idOrder)
-            deleteCart()
-        } else {
-            if(checkVoucher() == true) {
+        if(method_payment == "ship cod") {
+            if (voucher == "promo_01" || voucher == "") {
                 insertToCheckout()
                 val idOrder = insertToOrder()
                 insertToOrderDetails(idOrder)
                 deleteCart()
-            } else if(checkVoucher() == false) {
-                Toast.makeText(this, "Voucher Not Correct", Toast.LENGTH_SHORT).show()
-                insertToCheckout()
-                val idOrder = insertToOrder()
-                insertToOrderDetails(idOrder)
-                deleteCart()
+            } else {
+                if(checkVoucher() == true) {
+                    insertToCheckout()
+                    val idOrder = insertToOrder()
+                    insertToOrderDetails(idOrder)
+                    deleteCart()
+                } else if(checkVoucher() == false) {
+                    Toast.makeText(this, "Voucher Not Correct", Toast.LENGTH_SHORT).show()
+                    insertToCheckout()
+                    val idOrder = insertToOrder()
+                    insertToOrderDetails(idOrder)
+                    deleteCart()
+                }
             }
+            val intent = Intent(this@CheckoutActivity, HomeActivity::class.java)
+            startActivity(intent)
+            Toast.makeText(this, "Success to order", Toast.LENGTH_SHORT).show()
+        } else if(method_payment == "Razopay") {
+            paymentRazopay()
         }
-        val intent = Intent(this@CheckoutActivity, HomeActivity::class.java)
-        startActivity(intent)
-        Toast.makeText(this, "Success to order", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun paymentRazopay() {
+        val checkout = Checkout()
+        checkout.setKeyID("rzp_test_zHf96fMn8W3lq2");
+
+        val activity: Activity = this
+
+        try {
+            val options = JSONObject()
+            options.put("name", "Razorpay Corp")
+            options.put("description", "Demoing Charges")
+            //You can omit the image option to fetch the image from the dashboard
+            options.put("image", "https://s3.amazonaws.com/rzp-mobile/images/rzp.jpg")
+            options.put("theme.color", "#3399cc");
+            options.put("currency", "INR");
+//            options.put("order_id", "order_DBJOWzybf0sJbb");
+            var moneyTotal = 0.0
+            if(discountPercent != 0f) {
+                 moneyTotal = (total*discountPercent) / 100
+            } else {
+                moneyTotal = total
+            }
+            options.put("amount", "${moneyTotal.toInt()}")//amountx100
+            Log.d("AMOUNT", "${moneyTotal.toInt()}")
+            val retryObj = JSONObject();
+            retryObj.put("enabled", true);
+            retryObj.put("max_count", 4);
+            options.put("retry", retryObj);
+
+            val prefill = JSONObject()
+            prefill.put("email", "gaurav.kumar@example.com")
+            prefill.put("contact", "0774557680")
+            options.put("prefill", prefill)
+            checkout.open(activity, options)
+        } catch (e: Exception) {
+            Toast.makeText(activity, "Error in payment: " + e.message, Toast.LENGTH_LONG).show()
+            e.printStackTrace()
+        }
     }
 
     private fun checkVoucher(): Boolean {
         val cursor = contentResolver.query(uri_PromoCode, null, "idPromoCode = ?", arrayOf(voucher), null)
         if (cursor != null) {
             if (cursor.moveToFirst()) {
-                discountPercent = cursor.getFloat(cursor.getColumnIndexOrThrow("discountPercent"))
-                idPromoCode = cursor.getString(cursor.getColumnIndexOrThrow("idPromoCode"))
+                discountPercent = cursor.getFloat(cursor.getColumnIndexOrThrow("discountPercent")) ?: 0F
+                idPromoCode = cursor.getString(cursor.getColumnIndexOrThrow("idPromoCode")) ?: "promo_01"
                 return true
             }
             if (cursor != null) {
@@ -198,14 +255,16 @@ class CheckoutActivity : AppCompatActivity() {
     private fun insertToCheckout() {
         val user = getUserByToken()
         val values = ContentValues().apply {
-            put("recipientName", user!!.fullName)
+            put("recipientName", nameCheckOut)
             put("recipientEmail", user!!.email)
+            put("recipientPhoneNumber", phoneCheckOut)
             put("recipientAddress", addressCheckOut)
             if(discountPercent != 0f) {
                 put("total", ((total*discountPercent)/100))
             } else {
                 put("total", total)
             }
+            put("namePayment", method_payment)
             put("idAccount", getCurrentIdAccount())
         }
         val uri_user_checkout = contentResolver.insert(uri_checkout, values)
@@ -217,12 +276,13 @@ class CheckoutActivity : AppCompatActivity() {
             put("deliveryCharges", deliveryCharges)
             if(discountPercent != 0f) {
                 put("total", ((total*discountPercent)/100))
+                put("idPromocode", idPromoCode)
             } else {
                 put("total", total)
+                put("idPromocode", "promo_01")
             }
             put("idAccount", getCurrentIdAccount())
-            put("idPayment", "pay01")
-            put("idPromocode", "promo_01")
+            put("namePayment", method_payment)
         }
 
         val uri_user_order = contentResolver.insert(uri_order, values_order)
@@ -312,5 +372,42 @@ class CheckoutActivity : AppCompatActivity() {
 
         }
         return null
+    }
+
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            // Is the button now checked?
+            val checked = view.isChecked
+
+            // Check which radio button was clicked
+            when (view.getId()) {
+                R.id.radio_lock ->
+                    if (checked) {
+                        method_payment = "ship cod"
+                    }
+                R.id.radio_unlock ->
+                    if (checked) {
+                        method_payment = "Razopay"
+                    }
+            }
+        }
+    }
+
+    override fun onPaymentSuccess(p0: String?) {
+       try {
+           insertToCheckout()
+           val idOrder = insertToOrder()
+           insertToOrderDetails(idOrder)
+           deleteCart()
+           val intent = Intent(this@CheckoutActivity, HomeActivity::class.java)
+           startActivity(intent)
+           Toast.makeText(this, "Success to Order By Razopay", Toast.LENGTH_SHORT).show()
+       } catch (e: Exception) {
+           Toast.makeText(this, e.message.toString(), Toast.LENGTH_SHORT).show()
+       }
+    }
+
+    override fun onPaymentError(p0: Int, p1: String?) {
+        Toast.makeText(this, "Faild to Order due to ${p1.toString()} ", Toast.LENGTH_SHORT).show()
     }
 }
